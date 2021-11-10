fa36e56c21f7fd4529d323405b4430690db69108
Tiago-Sant <tiagocsantana10@gmail.com>
2021-11-10 01:36:43 -0300

    Github Explorer completed

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fundamentos do React - Rocketseat
+
+## Github Explorer
+
+Aplicação que traz uma lista dos meus repositórios criada em React.js, utilizando Typescript.
---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,20 @@
+type RepositoryItemProps = {
+  repository: {
+    name: string
+    description: string
+    html_url: string
+  }
+}
+
+export function RepositoryItem({ repository }: RepositoryItemProps) {
+  return (
+    <li>
+      <strong>{repository.name}</strong>
+      <p>{repository.description}</p>
+
+      <a href={repository.html_url} target="_blank">
+        Acessar repositórios
+      </a>
+    </li>
+  )
+}
